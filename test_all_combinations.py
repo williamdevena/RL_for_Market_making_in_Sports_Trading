808219b10524179713cb3bc4108a9ecf513395c2
williamdevena <william98wdv@gmail.com>
2023-09-09 12:40:51 +0100

    Modified parameter passing in test_all_comb and test_corr

--- test_all_combinations.py
+++ test_all_combinations.py
@@ -1,60 +1,52 @@
-"""
-This module runs simulations on all possible combinations of environment parameters using a trained RL
-agent and save plots of the distributions of various risk and performance metrics.
-"""
-
 from stable_baselines3 import A2C, DQN, PPO
 
 from testing import testing
 from utils import setup
 
 
-def main():
+def test_all_combinations(model_name,
+                          num_simulations_per_combination,
+                          mode,
+                          plot_path):
+    """
+    Runs simulations on all possible combinations of environment parameters using a trained RL
+    agent and save plots of the distributions of various risk and performance metrics.
+    """
     _ = setup.setup()
 
-    # model_name = "fixed_02"
-    # model = model_name
+    ### RL AGENTS
+    if model_name=="A2C":
+        model = A2C.load(f"./model_weights/{model_name}")
+    elif model_name=="DQN":
+        model = DQN.load(f"./model_weights/{model_name}")
+    elif model_name=="PPO":
+        model = PPO.load(f"./model_weights/{model_name}")
+    ### BASELINE MODELS
+    elif model_name in ["fixed_02", "fixed_05", "fixed_08", "random"]:
+        model = model_name
+    else:
+        raise ValueError(f"Invalid value {model_name} for variable 'model_name'. Should be either 'A2C', 'DQN' or 'PPO' (for RL agents) or 'fixed_02', 'fixed_05', 'fixed_08', 'random' (for the baseline models).")
 
-    # model_name = "fixed_05"
-    # model = model_name
-
-    # model_name = "fixed_08"
-    # model = model_name
-
-    # model_name = "random"
-    # model = model_name
-
-    # model_name = "PPO_1_k_4"
-    # model = PPO.load(f"./model_weights/with_k_4/PPO/{model_name}")
-
-    # model_name = "A2C_1_k_4"
-    # model = A2C.load(f"./model_weights/with_k_4/A2C/{model_name}")
-
-    # model_name = "DQN_1_k_4"
-    # model = DQN.load(f"./model_weights/with_k_4/DQN/{model_name}")
-
-    # model_name = "A2C_2_k_4"
-    # model = A2C.load(f"./model_weights/with_k_4/A2C/{model_name}")
-
-    # model_name = "DQN_2_k_4"
-    # model = DQN.load(f"./model_weights/with_k_4/DQN/{model_name}")
-
-    # model_name = "PPO_2_k_4"
-    # model = PPO.load(f"./model_weights/with_k_4/PPO/{model_name}")
-
-    model_name = "PPO_3_k_4"
-    model = PPO.load(f"./model_weights/with_k_4/PPO/{model_name}")
-
-
-
-    plot_path = f"./plots_test_2/{model_name}"
-    num_simulations_per_combination = 1
-    mode = "long" ## if 'short' uses a smaller set of possible combinations (used for test)
     testing.test_rl_agent_all_combinations(model=model,
-                                   num_simulations_per_combination=num_simulations_per_combination,
-                                   plot_path=plot_path,
-                                   mode=mode
-                                   )
+                                            num_simulations_per_combination=num_simulations_per_combination,
+                                            plot_path=plot_path,
+                                            mode=mode
+                                            )
+
+
+
+
+def main():
+    model_name = "PPO" ## possible values: A2C', 'DQN' or 'PPO' (for RL agents) or
+                       ## 'fixed_02', 'fixed_05', 'fixed_08', 'random' (for the baseline models)
+    plot_path = f"./plots_testing/{model_name}"
+    num_simulations_per_combination = 100
+    mode = "long" ## if 'short' uses a smaller set of possible combinations (used for test)
+    test_all_combinations(model_name=model_name,
+                          num_simulations_per_combination=num_simulations_per_combination,
+                          mode=mode,
+                          plot_path=plot_path)
+
 
 
 if __name__=="__main__":
--- test_correlation_state_actions.py
+++ test_correlation_state_actions.py
@@ -1,37 +1,48 @@
-"""
-This module runs simulations on all possible combinations of environment parameters with a given trained RL
-agent, and calculates and saves plots of the mean correlation matrix between state variables and actions.
-"""
-
 from stable_baselines3 import A2C, DQN, PPO
 
 from testing import testing
 from utils import setup
 
 
-def main():
+def test_correlations_state_actions(model_name,
+                                    mode,
+                                    num_simul_per_comb,
+                                    plot_path):
+    """
+    This module runs simulations on all possible combinations of environment parameters with a given trained RL
+    agent, and calculates and saves plots of the mean correlation matrix between state variables and actions.
+    """
     _ = setup.setup()
 
-    # model_name = "PPO_2_k_4"
-    # model = PPO.load(f"./model_weights/with_k_4/PPO/{model_name}")
+    if model_name=="A2C":
+        model = A2C.load(f"./model_weights/{model_name}")
+    elif model_name=="DQN":
+        model = DQN.load(f"./model_weights/{model_name}")
+    elif model_name=="PPO":
+        model = PPO.load(f"./model_weights/{model_name}")
+    else:
+        raise ValueError(f"Invalid value {model_name} for variable 'model_name'. Should be either 'A2C', 'DQN' or 'PPO'.")
 
-    # model_name = "PPO_3_k_4"
-    # model = PPO.load(f"./model_weights/with_k_4/PPO/{model_name}")
-
-    # model_name = "DQN_2_k_4"
-    # model = DQN.load(f"./model_weights/with_k_4/DQN/{model_name}")
-
-    model_name = "A2C_2_k_4"
-    model = A2C.load(f"./model_weights/with_k_4/A2C/{model_name}")
-
-
-    plot_path = f"corr_matr_actions_test/{model_name}"
-    num_simul_per_comb = 100
-    mode = "long" ## if 'short' uses a smaller set of possible combinations (used for test)
     testing.test_correlations_state_vars_and_actions(model=model,
                                                      mode=mode,
                                                      num_simul_per_combin=num_simul_per_comb,
                                                      plot_path=plot_path)
+
+
+
+def main():
+    model_name = "PPO" ## possible values: A2C', 'DQN' or 'PPO' (for RL agents) or
+                       ## 'fixed_02', 'fixed_05', 'fixed_08', 'random' (for the baseline models)
+    plot_path = f"corr_matr_actions_test/{model_name}"
+    num_simul_per_comb = 100
+    mode = "long" ## if 'short' uses a smaller set of possible combinations (used for test)
+
+    test_correlations_state_actions(model_name=model_name,
+                                    mode=mode,
+                                    num_simul_per_comb=num_simul_per_comb,
+                                    plot_path=plot_path)
+
+
 
 if __name__=="__main__":
     main()
